cfb35452987ad4a19dfa47f8a8d0c42b23d0a9e0
alanSechi <alan.sechi@gmail.com>
2021-02-24 01:17:44 +0100

    [ADD] adding the function to create the deck

--- scripts/deck.js
+++ scripts/deck.js
@@ -1,65 +1,23 @@
-class card {
-    constructor(worth, type, points) {
-      this.worth = worth;
-      this.type = type;
-      this.points = points;
-    }
-  }
-  
-  
-  let deck = [
-    new card("A", "diamonds",14),
-    new card("2", "diamonds",2),
-    new card("3", "diamonds",3),
-    new card("4", "diamonds",4),
-    new card("5", "diamonds",5),
-    new card("6", "diamonds",6),
-    new card("7", "diamonds",7),
-    new card("8", "diamonds",8),
-    new card("9", "diamonds",9),
-    new card("10", "diamonds",10),
-    new card("J", "diamonds",11),
-    new card("Q", "diamonds",12),
-    new card("K", "diamonds",13),
-    new card("A", "hearts",14),
-    new card("2", "hearts",2),
-    new card("3", "hearts",3),
-    new card("4", "hearts",4),
-    new card("5", "hearts",5),
-    new card("6", "hearts",6),
-    new card("7", "hearts",7),
-    new card("8", "hearts",8),
-    new card("9", "hearts",9),
-    new card("10", "hearts",10),
-    new card("J", "hearts",11),
-    new card("Q", "hearts",12),
-    new card("K", "hearts",13),
-    new card("A", "spades",14),
-    new card("2", "spades",2),
-    new card("3", "spades",3),
-    new card("4", "spades",4),
-    new card("5", "spades",5),
-    new card("6", "spades",6),
-    new card("7", "spades",7),
-    new card("8", "spades",8),
-    new card("9", "spades",9),
-    new card("10", "spades",10),
-    new card("J", "spades",11),
-    new card("Q", "spades",12),
-    new card("K", "spades",13),
-    new card("A", "clubs",14),
-    new card("2", "clubs",2),
-    new card("3", "clubs",3),
-    new card("4", "clubs",4),
-    new card("5", "clubs",5),
-    new card("6", "clubs",6),
-    new card("7", "clubs",7),
-    new card("8", "clubs",8),
-    new card("9", "clubs",9),
-    new card("10", "clubs",10),
-    new card("J", "clubs",11),
-    new card("Q", "clubs",12),
-    new card("K", "clubs",13),
-  ];
-  
-export  {deck} 
+
+var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var suits = ["diamonds", "hearts", "spades", "clubs"];
+var points = ["14", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+var deck = new Array();
+
+function getDeck()
+{
+	var deck = new Array();
+
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < cards.length; x++)
+		{
+			var card = {worth: cards[x], type: suits[i], points: points[x]};
+			deck.push(card);
+		}
+	}
+
+	return deck;
+}
+
+export { getDeck };
